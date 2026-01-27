9d187fe63cb313224b2887c683b6e5afc037f2b7
Athmiya Gowda <athmiyagowda39@gmail.com>
2026-01-27 05:28:25 +0000

    Try fixing this error: `Console Error: [2026-01-27T05:27:26.865Z] @fireb

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { AuthProvider } from '@/context/auth-context';
 import { UsersProvider } from '@/context/users-context';
-import LeadMigrator from '@/components/layout/lead-migrator';
 
 export const metadata: Metadata = {
   title: 'Sales Lead Tracking Dashboard',
@@ -33,7 +32,6 @@ export default function RootLayout({
       <body className="font-body antialiased">
         <UsersProvider>
           <AuthProvider>
-            <LeadMigrator />
             {children}
           </AuthProvider>
         </UsersProvider>
