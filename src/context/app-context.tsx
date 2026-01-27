8f3129d7dd2d1e6b7d12af8899efd8ba3f97d73f
Athmiya Gowda <athmiyagowda39@gmail.com>
2026-01-27 12:32:59 +0000

    replace the luke rajkumar name above the below given image

--- src/context/app-context.tsx
+++ src/context/app-context.tsx
@@ -36,11 +36,11 @@ type AppContextType = {
 };
 
 const defaultUsers: AppUser[] = [
+  { id: 'user-2', username: 'Luke Rajkumar', email: 'Luke.rajkumar@peopleworks.in', role: 'Super Admin', password: 'Luke@123', phoneNumber: '9500038277' },
   { id: 'user-1', username: 'Athmiya A G', email: 'athmiya.ag@peopleworks.in', role: 'Super Admin', password: 'Welcome123#' },
   { id: 'user-3', username: 'Varghese Vincent', email: 'Varghese@peopleworks.in', role: 'Super Admin', password: 'Varghese@123', phoneNumber: 'N/A' },
   { id: 'user-4', username: 'Sam Devasia', email: 'sam.devasia@peopleworks.in', role: 'Super Admin', password: 'SamDev@456', phoneNumber: 'N/A' },
   { id: 'user-6', username: 'Mandanna N', email: 'mandanna.n@peopleworks.in', role: 'Executive', password: 'Mandanna@101', phoneNumber: '9845622777' },
-  { id: 'user-2', username: 'Luke Rajkumar', email: 'Luke.rajkumar@peopleworks.in', role: 'Super Admin', password: 'Luke@123', phoneNumber: '9500038277' },
   { id: 'user-5', username: 'Yathish G', email: 'yathish.g@peopleworks.in', role: 'Executive', password: 'Yathish@789', phoneNumber: '8553309892' },
   { id: 'user-7', username: 'Hukum Chand Kewat', email: 'hukum@peopleworks.in', role: 'Executive', password: 'Hukum@112', phoneNumber: '9036010968' },
   { id: 'user-8', username: 'Hemant Sharma', email: 'hemant.sharma@peopleworks.in', role: 'Executive', password: 'Password123' },
